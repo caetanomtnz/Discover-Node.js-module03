1ec0890341fd066d746b6e6b5569d1fee4af7540
caetanomtnz <caetanomartinazzo25@gmail.com>
2021-06-09 10:26:56 -0400

    Add files via upload

---
+++ BACKEND-NODEJS/console.js
@@ -0,0 +1,4 @@
+// local
+console.log(__dirname)
+// caminho completo / nome do arquivo
+console.log(__filename)
---
+++ require.js
@@ -0,0 +1,8 @@
+//modulos nativos
+const path = require('path')
+
+console.log(path.basename("/Users/starter/docs.js"))
+//meus modulos
+const myModule = require('./exports')
+
+console.log(myModule)
